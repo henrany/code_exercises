a3cff07d243ee57ecb68e388ff053102aac9dde3
henrany <tmkhenry@gmail.com>
2020-03-31 14:51:25 -0300

    done

---
+++ uricodes/abovemdiogonal.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+
+int main(){
+    std::string carac;
+    int mat[12][12];
+    double sum=0,ave;
+    std::cin>>carac;
+    for(int i=0;i<12;i++){
+        for(int j=0;j<12;j++){
+            std::cin>>mat[i][j];
+        }
+    }
+
+    for(int i=0;i<12;i++){
+        for(int j=0;j<12;j++){
+            if(i<j){
+                sum+=mat[i][j];
+                ave=sum/66;
+            }
+        }
+    }
+
+    if(carac=="S"){
+        std::cout<<std::fixed<<std::showpoint;
+        std::cout<<std::setprecision(1);
+        std::cout<<sum<<std::endl;
+    }
+    else if(carac=="M"){
+        std::cout<<std::fixed<<std::showpoint;
+        std::cout<<std::setprecision(1);
+        std::cout<<ave<<std::endl;
+    }
+    else{
+        exit(1);
+    }
+    return 0;
+}
