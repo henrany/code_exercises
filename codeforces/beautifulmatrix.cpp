f04a5b9b8f355ea1279e54306c3a48d21fa5479f
henrany <tmkhenry@gmail.com>
2020-05-14 22:38:49 -0300

    codeforces problem 263A done

--- codeforces/beautifulmatrix.cpp
+++ codeforces/beautifulmatrix.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
     int arr[6][6];
-    int pos1=0,min=0,pos2=0;
+    int min=0;
     for(int i=1;i<=5;i++){
         for(int j=1;j<=5;j++){
             cin>>arr[i][j];
