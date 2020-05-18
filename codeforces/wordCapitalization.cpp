0b8e6a7d92d96fd4dc76270bcd51c0d512dee724
henrany <tmkhenry@gmail.com>
2020-05-18 17:09:20 -0300

    codeforces problem 281A solved

---
+++ codeforces/wordCapitalization.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    int c1=0,c2=0;
+    getline(cin,s);
+    s[0] = toupper(s[0]);
+    cout<<s<<"\n";
+    return 0;
+}
