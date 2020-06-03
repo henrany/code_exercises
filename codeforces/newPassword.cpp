1cefa19d81c3e18816ac746396107437926c7e3e
henrany <tmkhenry@gmail.com>
2020-06-03 03:42:46 -0300

    problem 770A done

--- codeforces/newPassword.cpp
+++ codeforces/newPassword.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(){
     vector<char> v{'a','b','c','d','e','f','g','h','i','j','k','l','m',
     'n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    unordered_set<char> s;
     int n,k,c=0;
     cin>>n>>k;
     for(int i=0;i<n;i++){
